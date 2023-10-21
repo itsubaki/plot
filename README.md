65fa888ee75248bf7d857e0edb454bc55eb1bb9c
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-10-22 00:33:00 +0900

    Add some files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # plot
-A Command Line Tool for plot
+
+ * A Command Line Tool for plot
+
+
+```shell
+$ go install github.com/itsubaki/plot
+```
+
+```shell
+$ plot testdata/cos.csv
+```
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, r := range records {
+		fmt.Println(r)
+	}
+}
